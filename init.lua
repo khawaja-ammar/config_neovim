e12cfb0e6fe220024de5259cc63490e218490abe
khawaja-ammar <mammarkh@gmail.com>
2023-12-14 13:16:22 +0500

    Added Commenting plugin

--- init.lua
+++ init.lua
@@ -74,16 +74,38 @@ local plugins = {
     end
   },
   -- CORE:Treesitter Ext ---------------------------------------
-  { "windwp/nvim-ts-autotag" },
-  { "RRethy/nvim-treesitter-endwise" },
-  -- EXT -------------------------------------------------------
   {
     "altermo/ultimate-autopair.nvim", 
     event = {'InsertEnter', 'CmdlineEnter'},
     config = function()
       require("ultimate-autopair").setup()
     end
+  }, 
+  { "windwp/nvim-ts-autotag" },
+  { "RRethy/nvim-treesitter-endwise" },
+  { 
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = false,
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        toggler = {
+          line = '<C-_>',
+          -- block = '<leader>bc',
+        },
+        opleader = {
+          line = '<C-_>',
+          -- block = '<leader>bc'
+        },
+      })
+    end
   },
+  -- EXT -------------------------------------------------------
+  
   -- INTEGRATIONS ----------------------------------------------
   {
     "kdheepak/lazygit.nvim",
