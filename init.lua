97fefb6da154a310b8b1468eba40f07288c09a14
khawaja-ammar <mammarkh@gmail.com>
2023-12-14 14:10:07 +0500

    organized

--- init.lua
+++ init.lua
@@ -43,26 +43,6 @@ local plugins = {
       require("lualine").setup()
     end
   },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-          vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame)
-          vim.keymap.set('n', '<leader>gd', gs.preview_hunk)
-        end
-      })
-      require("which-key").register({
-        g = {
-          name = "Git",
-          b = {"Git Blame Toggle"},
-          d = {"Git Diff"},
-        }
-      }, { prefix = "<leader>" })
-    end
-  },
   -- CORE EXT ---------------------------------------------------
   {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
@@ -138,6 +118,26 @@ local plugins = {
     opts = {}
   }, 
   -- INTEGRATIONS ----------------------------------------------
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame)
+          vim.keymap.set('n', '<leader>gd', gs.preview_hunk)
+        end
+      })
+      require("which-key").register({
+        g = {
+          name = "Git",
+          b = {"Git Blame Toggle"},
+          d = {"Git Diff"},
+        }
+      }, { prefix = "<leader>" })
+    end
+  },
   {
     "kdheepak/lazygit.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
