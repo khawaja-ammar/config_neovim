6302a76bba5ff40ecb54278b17e3dcb06ae26ca2
khawaja-ammar <mammarkh@gmail.com>
2023-12-15 12:57:35 +0500

    neo tree config

--- lua/plugins/ui/neo-tree.lua
+++ lua/plugins/ui/neo-tree.lua
@@ -8,10 +8,16 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>b', ':Neotree toggle<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem action=show left reveal toggle<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>bb', ':Neotree filesystem action=show left reveal toggle<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>bf', ':Neotree filesystem reveal float<CR>', { noremap = true, silent = true })
+
     require("which-key").register({
-      b = "Toggle File Tree"
+      b = {
+        name = "FileTree",
+        b = {"Toggle Filetree"},
+        f = {"Float Filetree"}
+      }
     }, { prefix = "<leader>" })
 
   end
