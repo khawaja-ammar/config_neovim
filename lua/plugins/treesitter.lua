5434d304a9e4e1f5190c103f0a443e7134636672
khawaja-ammar <mammarkh@gmail.com>
2023-12-17 20:35:47 +0500

    treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,43 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "typescript", "tsx", "astro", "css", "gitignore", "go","html", "json", "python","rust", "sql"},
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "diff",
+        "dockerfile",
+        "html",
+        "lua",
+        "luadoc",
+        "javascript",
+        "jsdoc",
+        "typescript",
+        "tsx",
+        "astro",
+        "css",
+        "gitignore",
+        "go",
+        "json",
+        "python",
+        "rust",
+        "sql",
+        "regex",
+        "markdown",
+        "markdown_inline",
+        "regex",
+      },
       highlight = { enable = true },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>"
+        }
+      }
     })
   end
 }
