90a359245a1caa9e85895e51893abe9c0648bf02
khawaja-ammar <mammarkh@gmail.com>
2023-12-18 23:01:59 +0500

    better keybinds + telescope

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Neovim Config
 
 Basic neovim config experimenting with different plugins
+
+# Keybinds
+
+## <C->
+- C-p => Git Files Explorer
+- C-_(/)  => Comment Line(s) 
+
+## <leader>p(roject)
+
+- pe => Project Explorer
+- pf => Project Files
+- ps => Project Grep search
+
+## <leader>g(it)
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,27 @@
 return {
-  'nvim-telescope/telescope.nvim', branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { noremap = true, silent = true })
-    
-    require("which-key").register({
-      f = {
-        name = "Find",
-        f = {"Find File"},
-        g = {"Find Item (Grep)"},
-      }
-    }, { prefix = "<leader>" })
-  end
+  {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<C-p>', builtin.git_files, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>ps', builtin.live_grep, { noremap = true, silent = true })
+
+      require("which-key").register({
+        p = {
+          name = "Project",
+          f = {"Project Files"},
+          s = {"Project Search (Grep)"},
+        }
+      }, { prefix = "<leader>" })
+    end
+  },
+  { 
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    config = function()
+      require("telescope").load_extension('fzf')
+    end
+  },
 }
