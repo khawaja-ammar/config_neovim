a350df10e96a4c42203255f75b3977a395b26449
khawaja-ammar <mammarkh@gmail.com>
2023-12-19 21:06:56 +0500

    keymaps separate now

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -1,5 +1,5 @@
 -- Toggle wrap
-vim.keymap.set('n', '<A-z>', ':set wrap<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-z>', ':set wrap!<CR>', { noremap = true, silent = true })
 
 -- Commands to switch neovim panes
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { noremap = true, silent = true })
