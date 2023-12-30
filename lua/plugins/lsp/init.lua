b665a623663726617dc3fca91cf8fce61d652e34
khawaja-ammar <mammarkh@gmail.com>
2023-12-30 19:55:13 +0500

    lsp configs split correctly

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,8 @@
 return {
   -- require('plugins/lsp/lsp-zero'),
-  require('plugins/lsp/lspconfig'),
+  require('plugins/lsp/lsp-config'),
+  -- require('plugins/lsp/lsp-config-from-scratch'),
+
   -- require('plugins/lsp/formatting-conform.nvim'),
   -- require('plugins/lsp/linting-nvim-lint'),
 }
@@ -9,41 +11,4 @@ return {
 
 
 
--- return {
---   'neovim/nvim-lspconfig',
---   event = "VeryLazy",
---   dependencies = {
---     'williamboman/mason.nvim',
---     'williamboman/mason-lspconfig.nvim',
---     -- Useful status updates for LSP
---     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
---     { 'j-hui/fidget.nvim', opts = {} },
---
---     -- Additional lua configuration, makes nvim stuff amazing!
---     -- 'folke/neodev.nvim',
---   },
---   config = function()
---     require("mason").setup({
---       ui = {
---         icons = {
---           package_installed = "✓",
---           package_pending = "➜",
---           package_uninstalled = "✗"
---         }}
---     })
---
---     require("mason-lspconfig").setup({
---       ensure_installed = { "lua_ls" }
---     })
---
---     -- require('neodev').setup()
---
---     local lspconfig = require("lspconfig")
---     lspconfig.lua_ls.setup({})
---
---     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
---     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
---     vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction"})
---   end
--- }
 
