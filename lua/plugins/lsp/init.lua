3f5e982e9c20f6b12b3d04db3af3d7fd9c298a0e
khawaja-ammar <mammarkh@gmail.com>
2023-12-30 21:22:45 +0500

    added nvim lint basic

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,12 +2,6 @@ return {
   -- require('plugins/lsp/lsp-zero'),
   require('plugins/lsp/lsp-config'),
   -- require('plugins/lsp/lsp-config-from-scratch'),
-  require('plugins/lsp/formatting-conform'),
-  -- require('plugins/lsp/linting-nvim-lint'),
+  require('plugins/lsp/formatter-conform'),
+  require('plugins/lsp/linter-nvim-lint'),
 }
-
-
-
-
-
-
