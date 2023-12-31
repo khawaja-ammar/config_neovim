44256c486039a3fa410e50402ae9b947bc1bfcde
khawaja-ammar <mammarkh@gmail.com>
2023-12-31 15:13:40 +0500

    Lsp config complete now

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -8,8 +8,8 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    -- vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal float<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem action=show left reveal toggle<CR>', { noremap = true, silent = true })
     -- vim.keymap.set('n', '<leader>bb', ':Neotree filesystem action=show left reveal toggle<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>pe', ':Neotree filesystem action=show left reveal toggle<CR>', { desc = '[P]roject [E]xplorer' })
+    vim.keymap.set('n', '<leader>pe', ':Neotree filesystem reveal float<CR>', { desc = '[P]roject [E]xplorer' })
   end,
 }
