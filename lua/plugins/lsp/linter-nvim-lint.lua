235ece9e18fd1147199915e5cf7cf9d46db651fa
khawaja-ammar <mammarkh@gmail.com>
2023-12-31 18:06:37 +0500

    added mason installer

--- lua/plugins/lsp/linter-nvim-lint.lua
+++ lua/plugins/lsp/linter-nvim-lint.lua
@@ -5,33 +5,35 @@ return {
     local lint = require('lint')
 
     lint.linters_by_ft = {
-      ['*'] = { 'cspell' },
+      -- ['*'] = { 'cspell' },
 
       javascript = { 'eslint_d' },
       typescript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
+
+      python = { 'pylint' },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
 
-    -- vim.keymap.set('n', '<leader>ll', function()
-    -- lint.try_lint()
-    -- end, { desc = 'Trigger linting for current file' })
+    vim.keymap.set('n', '<leader>l', function()
+      lint.try_lint()
+    end, { desc = 'Trigger linting for current file' })
 
-    vim.api.nvim_create_user_command('LintList', function()
-      local linters = require('lint').get_running()
-      if #linters == 0 then
-        return '󰦕'
-      end
-      return '󱉶 ' .. table.concat(linters, ', ')
-    end, { desc = 'List available linter for buffer' })
+    -- vim.api.nvim_create_user_command('LintList', function()
+    --   local linters = require('lint').get_running()
+    --   if #linters == 0 then
+    --     return '󰦕'
+    --   end
+    --   return '󱉶 ' .. table.concat(linters, ', ')
+    -- end, { desc = 'List available linter for buffer' })
   end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,9 @@
 return {
   'williamboman/mason.nvim',
   event = 'VeryLazy',
+  dependencies = {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
   config = function()
     require('mason').setup({
       ui = {
@@ -11,6 +14,29 @@ return {
         },
       },
     })
-    vim.api.nvim_command('MasonUpdate')
+
+    require('mason-tool-installer').setup({
+      run_on_start = true,
+      ensure_installed = {
+        -- Formatters
+        'beautysh',
+        'sql-formatter',
+        'stylua',
+        'prettierd',
+        'prettier',
+        'black',
+        -- Linters
+        'biome',
+        'eslint_d',
+        'pylint',
+        -- LSP
+        'lua_ls',
+        'tailwindcss',
+        'tsserver',
+        'pyright',
+      },
+      auto_update = true,
+    })
+    vim.api.nvim_command('MasonToolsUpdate')
   end,
 }
