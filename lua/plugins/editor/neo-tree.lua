354d6249a85e6ac93327a5a501db50261117b738
khawaja-ammar <mammarkh@gmail.com>
2024-01-02 15:32:35 +0500

    minor

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -12,7 +12,7 @@ return {
     require('neo-tree').setup({
       filesystem = {
         filtered_items = {
-          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          -- visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = false,
           hide_gitignored = true,
           hide_by_name = {
